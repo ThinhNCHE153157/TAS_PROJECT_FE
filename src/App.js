8b40a03adc67424236fd5099de4e459888f05fc7
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-10-20 05:32:08 +0700

    add prettier config, add layout and page Register

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import Header from "./component/layout/Header";
-import Footer from "./component/layout/Footer";
-import Sidebar from "./component/pages/HOD/layout/Sidebar";
-import { BrowserRouter as Routers } from "react-router-dom";
-import Userdetail from "./component/pages/HOD/UserDetail";
-import TheRouter from "./component/router/router";
+import Header from './component/layout/Header';
+import Footer from './component/layout/Footer';
+import Sidebar from './component/pages/HOD/layout/Sidebar';
+import { BrowserRouter as Routers } from 'react-router-dom';
+import Userdetail from './component/pages/HOD/UserDetail';
+import TheRouter from './component/router/router';
 function App() {
-  return (
-    <div className="App">
-      {/* <Header />
+    return (
+        <div className="App">
+            {/* <Header />
       <Footer /> */}
-      {/* <Sidebar /> */}
-      <demor />
-      <Routers>
-        <TheRouter /> Use the renamed component here
-      </Routers>
-    </div>
-  );
+            {/* <Sidebar /> */}
+            <demor />
+            <Routers>
+                <TheRouter /> {/*Use the renamed component here */}
+            </Routers>
+        </div>
+    );
 }
 
 export default App;
--- src/component/router/router.jsx
+++ src/component/router/router.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Userdetail from '../pages/HOD/UserDetail'
-import Header from '../layout/Header'
-
-
-
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Userdetail from '../pages/HOD/UserDetail';
+import Header from '../layout/Header';
+import Register from '../pages/commonUser/pages/Register';
 
 const TheRouter = () => {
-  return (
-    <Routes>
-      {/* <Route path='' exact element={<RouterHere />} /> */}
-      {/* <Route path='/header' exact element={<Header />} /> */}
-      <Route path='/Student' exact element={<Header />} />
-      <Route path='/face' exact element={<Userdetail />} />
+    return (
+        <Routes>
+            {/* <Route path='' exact element={<RouterHere />} /> */}
+            {/* <Route path='/header' exact element={<Header />} /> */}
+            <Route path="/Student" exact element={<Header />} />
+            <Route path="/face" exact element={<Userdetail />} />
+            <Route path="/commonUser/register" exact element={<Register />} />
+        </Routes>
+    );
+};
 
-
-    </Routes>
-  )
-}
-
-export default TheRouter
+export default TheRouter;

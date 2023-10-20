7e86c849117ac7ff55b2cb3cfcce80827c5362c6
PhamTienDat-HE150862 <datpthe150862@fpt.edu.vn>
2023-10-20 21:53:18 +0700

    user profile u1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Header from "./component/layout/Header";
 import Footer from "./component/layout/Footer";
 import Sidebar from "./component/pages/HOD/layout/Sidebar";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserProfile from "./component/pages/UserProfile/UserProfile";
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* <Header />
-      <Footer /> */}
-      <Sidebar />
+      <Footer /> 
+      <Sidebar />*/}
+      
+      <UserProfile />
     </div>
+    
   );
 }
 

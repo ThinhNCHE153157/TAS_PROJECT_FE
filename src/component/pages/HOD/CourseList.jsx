a950df683e5ea06d07f442bb6e6dbfc873005f28
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-10-27 22:11:59 +0700

    Add course management in admin dashboard

--- src/component/pages/HOD/CourseList.jsx
+++ src/component/pages/HOD/CourseList.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { Avatar, Box } from '@mui/material'
 import Sidebar from './layout/Sidebar'
 import NavBar from './layout/NavBar'
@@ -6,8 +6,8 @@ import DataGridBase from '../common/DataGridBase'
 
 const statusOptions = {
   0: 'Hoạt động',
-  1: 'Chờ xử lý',
-  2: 'Ngừng hoạt động',
+  //1: 'Chờ xử lý',
+  1: 'Ngừng hoạt động',
 };
 
 const getValueOption = (value) => {
@@ -32,23 +32,15 @@ const getColor = (value) => {
 }
 const CourseList = () => {
   const columns = useMemo(() => [
-    {
-      field: 'avatar', headerName: 'Avatar', flex: 0.5,
-      renderCell: params => <Avatar src={params.row.avatar} />,
-      sortable: false,
-      filterable: false,
-    },
-    { field: 'username', headerName: 'Username', flex: 1 },
-    // { field: 'username', headerName: 'Username', width: 100 },
-    { field: 'email', headerName: 'Email', flex: 1 },
-    { field: 'phone', headerName: 'Phone', flex: 1 },
-    {
-      field: 'name',
-      headerName: 'Name',
-      valueGetter: (params) =>
-        `${params.row.first_name || ''} ${params.row.last_name || ''}`,
-      flex: 1
-    },
+
+    { field: 'courseId', headerName: 'Course ID', flex: 1 },
+    { field: 'courseDescription', headerName: 'Course Description', flex: 1 },
+    { field: 'courseLevel', headerName: 'Course Level', flex: 1 },
+    { field: 'createUser', headerName: 'Create User', flex: 1 },
+    { field: 'updateUser', headerName: 'Update User', flex: 1 },
+    { field: 'createDate', headerName: 'Create Date', flex: 1 },
+    { field: 'updateDate', headerName: 'Update Date', flex: 1 },
+
     {
       field: 'status',
       headerName: 'Status',
@@ -57,8 +49,7 @@ const CourseList = () => {
       type: 'singleSelect',
       valueOptions: [
         { value: 0, label: 'Hoạt động' },
-        { value: 1, label: 'Chờ xử lý' },
-        { value: 2, label: 'Ngừng hoạt động' },
+        { value: 1, label: 'Ngừng hoạt động' },
       ],
       renderCell: (params) => {
         const color = getColor(params.value);
@@ -73,153 +64,34 @@ const CourseList = () => {
 
   ], [])
 
-  const data = [
-    {
-      data_id: 1,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    },
-    {
-      data_id: 2,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?2',
-      username: 'jane.doe',
-      email: 'jane.doe@email.com',
-      phone: '0123456780',
-      first_name: 'Jane',
-      last_name: 'Doe',
-      status: 1 // chờ xử lý
-    },
-    {
-      data_id: 3,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 4,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 5,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 6,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 7,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 8,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 9,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 10,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 11,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 12,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 13,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    }, {
-      data_id: 14,
-      avatar: 'https://source.unsplash.com/100x100/?portrait?1',
-      username: 'john.doe',
-      email: 'john.doe@email.com',
-      phone: '0123456789',
-      first_name: 'John',
-      last_name: 'Doe',
-      status: 0 // hoạt động
-    },
-  ];
+  const [listcourse, setListcourse] = React.useState([])
+  useEffect(() => {
+    async function getListcourse() {
+        const requestUrl = 'https://localhost:5000/api/Course/GetAllCourse';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        setListcourse(responseJSON); 
+  }
+  getListcourse();
+}, [])
 
-  const rows = data.map(item => {
-    const { data_id, ...otherFields } = item;
+  const rows = listcourse.map(item => {
+    const { courseId, ...otherFields } = item;
     return {
       ...otherFields,
-      id: data_id
+      courseId: courseId,
+      id: courseId
     };
   });
 
   return (
+
     <div>
       <NavBar />
       <Box sx={{ display: 'flex' }}>
         <Sidebar />
         <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-          <DataGridBase columns={columns} rows={rows} pageName='Course List' />
+          <DataGridBase columns={columns} rows={rows} pageName='Course List' /> 
         </Box>
       </Box>
     </div>

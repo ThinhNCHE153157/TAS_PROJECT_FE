3e2f4f00a975e9f411d17236edd850943d232ddd
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-11-05 00:38:04 +0700

    update

--- server_deploy.sh
+++ server_deploy.sh
@@ -1,5 +1,5 @@
 docker login ghcr.io -u thinhnche153157 -p $TAS_FE_TOKEN
-docker stop tas_fe || true  
-docker rm tas_fe || true  
-docker rmi ghcr.io/thinhnche153157/tas_fe:latest || true 
-docker run -d --name tas_fe --network main-proxy_default --restart on-failure:5 -p 3036:80 ghcr.io/thinhnche153157/tas_fe:latest
+docker stop tas_project_fe || true  
+docker rm tas_project_fe || true  
+docker rmi ghcr.io/thinhnche153157/tas_project_fe:latest || true 
+docker run -d --name tas_project_fe --network main-proxy_default --restart on-failure:5 -p 3036:80 ghcr.io/thinhnche153157/tas_project_fe:latest

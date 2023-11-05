269404b123f6f8707188645bef5201a849451e92
Nguyen Cong Thinh <145247570+ThinhNCHE153157@users.noreply.github.com>
2023-11-05 15:05:04 +0700

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -2,4 +2,4 @@ docker login ghcr.io -u thinhnche153157 -p $TAS_FE_TOKEN
 docker stop tas_project_fe || true  
 docker rm tas_project_fe || true  
 docker rmi ghcr.io/thinhnche153157/tas_project_fe:latest || true 
-docker run -d --name tas_project_fe --network main-proxy_default --restart on-failure:5 -p 3036:80 ghcr.io/thinhnche153157/tas_project_fe:latest
+docker run -d --name tas_project_fe --restart on-failure:5 -p 3036:80 ghcr.io/thinhnche153157/tas_project_fe:latest

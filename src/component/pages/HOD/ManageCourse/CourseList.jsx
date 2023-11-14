979b6fffc7761543a3a0fde7171c6061be620210
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-11-14 07:02:35 +0700

    view Test detail

--- src/component/pages/HOD/ManageCourse/CourseList.jsx
+++ src/component/pages/HOD/ManageCourse/CourseList.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo, useEffect } from 'react';
-import { Box, Button } from '@mui/material';
+import React, { useMemo, useEffect, useState } from 'react';
+import { Box, Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import Sidebar from '../layout/Sidebar';
 import NavBar from '../layout/NavBar';
 import DataGridBase from '../../common/DataGridBase';
@@ -35,9 +35,38 @@ const getColor = (value) => {
     }
 };
 const CourseList = () => {
+    const [refresh, setRefresh] = useState(false); // This is for refresh data after edit
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const [courseName, setCourse_Name] = useState('');
+    const [courseDescription, setCourse_Description] = useState('');
+    const [courseLevel, setCourse_Level] = useState('');
+    const handleClose = (e) => {
+        e.preventDefault();
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ courseName, courseDescription, courseLevel }),
+        };
+        fetch(`https://localhost:5000/api/Course/AddCourse`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+        setOpen(false);
+        setRefresh(!refresh);
+    };
+    const handleClosedialog = () => {
+        setOpen(false);
+    };
+
     const columns = useMemo(
         () => [
-            { field: 'courseId', headerName: 'Course ID', flex: 1 },
+            { field: 'courseName', headerName: 'Course Name', flex: 1 },
             { field: 'courseDescription', headerName: 'Course Description', flex: 1 },
             { field: 'courseLevel', headerName: 'Course Level', flex: 1 },
             { field: 'createUser', headerName: 'Create User', flex: 1 },
@@ -77,7 +106,7 @@ const CourseList = () => {
         [],
     );
 
-    const [listcourse, setListcourse] = React.useState([]);
+    const [listcourse, setListcourse] = useState([]);
     useEffect(() => {
         async function getListcourse() {
             const requestUrl = 'https://localhost:5000/api/Course/GetAllCourse';
@@ -86,7 +115,7 @@ const CourseList = () => {
             setListcourse(responseJSON);
         }
         getListcourse();
-    }, []);
+    }, [refresh]);
 
     const rows = listcourse.map((item) => {
         const { courseId, createDate, updateDate, ...otherFields } = item;
@@ -94,7 +123,6 @@ const CourseList = () => {
             ...otherFields,
             createDate: formatDate(createDate),
             updateDate: formatDate(updateDate),
-            courseId: courseId,
             id: courseId,
         };
     });
@@ -104,10 +132,53 @@ const CourseList = () => {
             <NavBar />
             <Box sx={{ display: 'flex' }}>
                 <Sidebar />
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <DataGridBase columns={columns} rows={rows} pageName="Course List" />
+                <Box component="main" sx={{ flexGrow: 1, p: 3, mt: "24px" }}>
+                    <React.Fragment >
+                        <Button sx={{ ml: '20px', mt: '30px' }} variant="outlined" onClick={handleClickOpen}>
+                            Add Courser
+                        </Button>
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle>Add Course</DialogTitle>
+                            <DialogContent>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Course Name"
+                                    type="email"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setCourse_Name(e.target.value)}
+                                />
+                                <TextField
+                                    margin="dense"
+                                    id="name"
+                                    label="Course Description"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setCourse_Description(e.target.value)}
+                                />
+                                <TextField
+                                    margin="dense"
+                                    id="name"
+                                    label="Course level"
+                                    type="number"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setCourse_Level(e.target.value)}
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClosedialog}>Cancel</Button>
+                                <Button onClick={handleClose}>Add</Button>
+                            </DialogActions>
+                        </Dialog>
+                    </React.Fragment>
+                    <DataGridBase columns={columns} rows={rows} />
                 </Box>
             </Box>
+
         </div>
     );
 };

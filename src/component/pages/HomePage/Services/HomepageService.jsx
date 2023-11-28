327d44f0b0c3d14cc250e668e1510480a47e7c9f
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-11-28 12:25:53 +0700

    show list test free

--- src/component/pages/HomePage/Services/HomepageService.jsx
+++ src/component/pages/HomePage/Services/HomepageService.jsx
@@ -1,3 +1,5 @@
+import { BASE_URL } from "../../../../Utils/Constants";
+
 const GetlistCourse = async () => {
     const requestOptions = {
         method: 'Get',
@@ -7,6 +9,16 @@ const GetlistCourse = async () => {
         .then(response => response.json())
         .then(data => data);
 };
+const GetlistTest = async () => {
+    const requestOptions = {
+        method: 'Get',
+        headers: { 'Content-Type': 'application/json' },
+    };
+    return fetch(`${BASE_URL}Test/getListTestFreeResponseDtos`, requestOptions)
+        .then(response => response.json())
+        .then(data => data);
+};
 export {
     GetlistCourse,
+    GetlistTest
 };

8ce2f4c3d91b4992b3e3717ee91d3ddec799164a
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-11-28 07:50:59 +0700

    update

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
-import { AppBar, Tabs, Tab, Toolbar, Button, useMediaQuery, useTheme, Grid } from '@mui/material'
+import { AppBar, Tabs, Tab, Toolbar, Button, useMediaQuery, useTheme, Box, Tooltip, IconButton, Avatar, Menu, Typography, MenuItem } from '@mui/material'
 import DrawerComponent from './DrawerComponent'
 import logo from '../../image/tải xuống (1).png'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 export default function Header() {
   const nav = useNavigate();
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   const auth = useSelector((state) => state.auth?.user);
   const [tabValue, setTabValue] = useState(0)
   const theme = useTheme();
@@ -39,7 +50,38 @@ export default function Header() {
                 <Button sx={{ marginLeft: 'auto' }} variant='outlined'><Link to="Login" color="inherit" underline="none">Login</Link></Button>
               </>
                 : <>
-                  <Button sx={{ marginLeft: 'auto' }} variant='outlined'><Link to="Login" color="inherit" underline="none">Logout</Link></Button>
+
+                  <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Open settings">
+                      <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                      </IconButton>
+                    </Tooltip>
+                    <Menu
+                      sx={{ mt: '45px' }}
+                      id="menu-appbar"
+                      anchorEl={anchorElUser}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(anchorElUser)}
+                      onClose={handleCloseUserMenu}
+                    >
+                      {settings.map((setting) => (
+                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                          {(setting === 'Logout') ? <Link to="/Login" color="inherit" underline="none">{setting}</Link>
+                            :
+                            <Typography textAlign="center">{setting}</Typography>}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </Box>
                 </>}
             </>
           )

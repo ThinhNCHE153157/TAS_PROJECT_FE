30e922c028c832db23d4a5f11d83b9b5c3240c0d
Nguyen Cong Thinh <thinhnche153157@fpt.edu.vn>
2023-12-01 16:14:21 +0700

    fix header

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -4,6 +4,7 @@ import DrawerComponent from './DrawerComponent'
 import logo from '../../Assets/img/Logo1.png'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { Padding } from '@mui/icons-material'
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 export default function Header() {
@@ -44,7 +45,7 @@ export default function Header() {
             </>
           ) : (
             <>
-              <img src={logo} alt='' width='5%' style={{ minWidth: '5%' }} />
+              <img src={logo} alt='' width='6%' style={{ minWidth: '5%', marginLeft: "7%" }} />
               <Tabs onChange={handleChange} value={tabValue} indicatorColor='primary' sx={{ marginLeft: 'auto' }}>
                 <Tab key={0} value={0} label='Khoá học online' onClick={() => { nav('/') }} />
                 <Tab key={1} value={1} label='Đề thi online' onClick={() => nav('/Test')} />

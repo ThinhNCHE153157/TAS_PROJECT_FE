b968e4bd8e3d5aad55045584d1982e1fb34fa538
vinhtrt17 <vinhtthe153663@fpt.edu.vn>
2023-12-22 16:33:59 +0700

    upload phe duyet

--- src/pages/Enterprise/ManageCourse/Step/FifthStep.jsx
+++ src/pages/Enterprise/ManageCourse/Step/FifthStep.jsx
@@ -1,29 +1,24 @@
-import { Box, Typography } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
 import React from 'react'
-
+import PublishIcon from '@mui/icons-material/Publish';
+import { ToastContainer } from 'react-toastify';
+import { alertSuccess, alertInfo } from '../../../../component/AlertComponent';
 const FifthStep = () => {
+  const notify = () => {
+    alertInfo({ message: "Khóa học của bạn đang chờ phê duyệt" });
+  }
   return (
     <Box width='100%'>
-      <Typography fontSize='22px' fontWeight='500'>
-        Giới thiệu khóa học
-      </Typography>
-      <Box display='flex' flexDirection='column'>
-        <Typography fontSize='16px' fontWeight='bold'>
-          Tiêu đề khóa học
+      <Box width='100%' display='flex' mt='3%' flexDirection='column' alignItems='center'>
+        <Typography fontSize='28px' ml='2%' mb='2%' fontWeight='bold'>
+          Xuất bản khóa học
         </Typography>
-
-        <Typography fontSize='16px' fontWeight='bold'>
-          Tiêu đề khóa học
-        </Typography>
-
-        <Typography fontSize='16px' fontWeight='bold'>
-          Tiêu đề khóa học
-        </Typography>
-
-        <Typography fontSize='16px' fontWeight='bold'>
-          Tiêu đề khóa học
-        </Typography>
-
+        <IconButton sx={{ m: '0 auto', bgcolor: 'green', borderRadius: '5px', width: '180px', mb: '5%' }} onClick={notify}>
+          <PublishIcon sx={{ color: 'white' }} />
+          <Typography fontSize='22px' fontWeight='bold' color='white' ml='1%' >
+            Xuất bản
+          </Typography>
+        </IconButton>
       </Box>
     </Box>
 
